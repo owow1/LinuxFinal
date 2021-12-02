92b9506421321910624e2c3182dd9d31e8fd6198
owow1 <1505545233@qq.com>
2021-12-02 22:48:18 +0800

    Text

--- Text.java
+++ Text.java
@@ -1,28 +1,44 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class App {
+@WebServlet(urlPatterns = "/Text")
+public class Text extends HttpServlet {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://106.12.47.180/linux_final";
     static final String USER = "root";
     static final String PASS = "Bb20010122@";
 
-    public static void main(String[] args) {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+          response.setContentType("text/html");
+          PrintWriter out = response.getWriter();
+
+          Student stu = getStudent();
+
+
+          out.println("<h1>hello world, " + stu.name + "</h1>");
+    }
+    
+    public Student getStudent() {
+        Student stu = new Student();
         Connection conn = null;
         Statement stmt = null;
         try {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             stmt = conn.createStatement();
-            String sql = "SELECT id,name FROM t_student WHERE id=4";
+            String sql = "SELECT * FROM t_student WHERE id=4";
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                System.out.printf("id: %d,name: %s",id,name);
+                stu.id = rs.getInt("id");
+                stu.name = rs.getString("name");
             }
             rs.close();
             stmt.close();
@@ -41,5 +57,17 @@ public class App {
                 se.printStackTrace();
             }
         }
+
+    return stu;
+   
     }
+
+}
+
+
+class Student {
+
+    public String name;
+    public int id;
+
 }
